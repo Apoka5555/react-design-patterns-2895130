29a6b19860912df585376fe525c2111c5da2e18a
Apoka5555 <apoka5555@gmail.com>
2023-01-24 14:53:24 +0600

    container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import axios from "axios";
+import { UserInfo } from "./UserInfo";
+import { DataSource } from "./DataSource";
+import { ProductInfo } from "./ProductInfo";
+
+const getServerData = (url) => async () => {
+  const response = await axios.get(url);
+  return response.data;
+};
+
 function App() {
-	return (
-		<h1>Your code goes here</h1>
-	);
+  return (
+    <>
+      <DataSource getDataFunc={getServerData("/users/123")} resourceName="user">
+        <UserInfo />
+      </DataSource>
+
+      <DataSource
+        getDataFunc={getServerData("/products/1234")}
+        resourceName="product"
+      >
+        <ProductInfo />
+      </DataSource>
+    </>
+  );
 }
 
 export default App;
